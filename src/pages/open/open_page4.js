8923f3896beb009103e7e2d8908148d0481ab217
EUNGtemplar <noblegaga@gmail.com>
2023-12-20 10:31:29 +0900

    open_1220

--- src/pages/open/open_page4.js
+++ src/pages/open/open_page4.js
@@ -13,18 +13,24 @@ const Open_page4 = () => {
             data-aos-easing="ease-in-out"
             data-aos-mirror="true"
             data-aos-once="false"
-            data-aos-anchor-placement="top"
+            data-aos-anchor-placement="top-center"
             className="typelt">
             <TypeIt
             
                 getBeforeInit={(instance) => {
-                instance.type("오래된 것들.").pause(750)
+                instance.type("오래된 것들.").pause(1050)
+                .delete(7).pause(500).type("중고.").pause(1050)
+                .delete(3).pause(500).type("새것.").pause(3200)
+                .delete(3).pause(500).type("오래된 것들.").pause(2400)
                 .delete(7).pause(500).type("중고.").pause(750)
-                .delete(3).pause(600).type("헌것, 폐품.").pause(750)
+                .delete(3).pause(500).type("불필요한.").pause(750)
+                .delete(5).pause(600).type("헌것, 폐품.").pause(750)
                 .delete(7).pause(500).type("쓸수없는.").pause(750)
                 .delete(6).pause(600).type("사용할 수 없는.").pause(750)
                 .delete(10).pause(600).type("고물.").pause(750)
-                .delete(3).pause(600).type().pause(500)
+                .delete(3).pause(600).type("used").pause(500)
+                .delete(4).pause(600).type("second hand").pause(500)
+                .delete(11).pause(600).type("'추억.'").pause(500)
                 ;
 
                 return instance}}
