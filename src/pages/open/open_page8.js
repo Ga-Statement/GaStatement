1a2e5f9b53e32504671f03427bfd8c638ac5086d
EUNGtemplar <noblegaga@gmail.com>
2023-12-21 15:17:50 +0900

    open_page_최최최종

--- src/pages/open/open_page8.js
+++ src/pages/open/open_page8.js
@@ -4,10 +4,10 @@ import { Link } from "react-router-dom";
 const Open_page8 = () => {
     return (
         <div>
-            <div className="open-title flex-col h-100vh">
-                <Link to="/main"><img src="/pic/open_pic/logo_1.png" alt="" /></Link>
+            <div className="open-title flex-col h-100vh bg-black">
+                <Link to="/main"><img src="/pic/open_pic/open_page_last.gif" alt="" /></Link>
                 <Link to="/main" style={{textDecoration: 'none'}}>
-                    <h5 className="font-size-20 font-lighter font-black margin-0 margin-top-30">서랍 열기</h5>
+                    <h5 className="font-size-20 font-lighter font-white">서랍 열기</h5>
                 </Link>
             </div>
         </div>
