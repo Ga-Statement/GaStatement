47741244eb5d12717878b8dc737f05a8c667dfaa
EUNGtemplar <noblegaga@gmail.com>
2023-12-21 14:49:22 +0900

    openpage_re2

--- src/pages/open/open_page7.js
+++ src/pages/open/open_page7.js
@@ -37,7 +37,7 @@ const expand = (item, i) => {
   })
   gsap.to(items, {
     width: item.clicked ? '15vw' : '10vw',
-    height: item.clicked ? '15vh' : '10vh',
+    height: item.clicked ? '30vh' : '25vh',
     duration: 2,
     ease: 'elastic(1, .6)'
   })
@@ -45,7 +45,7 @@ const expand = (item, i) => {
   item.clicked = !item.clicked
   gsap.to(item, {
     width: item.clicked ? '15vw' : '17vw',
-    height: item.clicked ? '15vh' : '45vh',
+    height: item.clicked ? '30vh' : '45vh',
     duration: 2.5,
     ease: 'elastic(1, .3)'
   })
@@ -126,7 +126,7 @@ items.forEach((item, i) => {
                     </div>
                     <div className="font-gray text-left">
                     <div class="item" style={{backgroundImage:'url("/pic/open_pic/002png.PNG")'}}>
-                        </div>
+                    <div className="page7_name"> &nbsp; &nbsp; 클릭하여 정보를 확인하세요!</div></div>
                         <h5 className="font-size-20 margin-0 margin-top-10"> 명품 전문가</h5>
                     </div>
                     <div className="font-gray text-left">
