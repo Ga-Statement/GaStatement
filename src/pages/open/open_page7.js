d3a20d1baf8e19c7c549809785c8ff9b29f1c6b6
EUNGtemplar <noblegaga@gmail.com>
2023-12-21 14:00:41 +0900

    openpage_re2

--- src/pages/open/open_page7.js
+++ src/pages/open/open_page7.js
@@ -78,7 +78,7 @@ items.forEach((item, i) => {
                                     data-aos-easing="ease-in-out"
                                     data-aos-mirror="true"
                                     data-aos-once="false"
-                                    data-aos-anchor-placement="bottom">서랍 서비스는 당신의 불편함과 불안함을 대신 합니다.</h3>
+                                    data-aos-anchor-placement="bottom">서랍은 당신의 불편함과 불안함을 대신 합니다.</h3>
                 </div>
                 <div
                                                     data-aos="fade-up"
@@ -117,12 +117,12 @@ items.forEach((item, i) => {
                     <div className="font-gray text-left">
                         <div class="item" style={{backgroundImage:'url("/pic/open_pic/005png.PNG")'}}>
                         </div>
-                        <h5 className="font-size-20 margin-0 margin-top-10">가구 전문가</h5>
+                        <h5 className="font-size-20 margin-0 margin-top-10">음악 전문가</h5>
                     </div>
                     <div className="font-gray text-left">
                     <div class="item" style={{backgroundImage:'url("/pic/open_pic/004png.PNG")'}}>
                         </div>
-                        <h5 className="font-size-20 margin-0 margin-top-10">명품 전문가</h5>
+                        <h5 className="font-size-20 margin-0 margin-top-10">도서 전문가</h5>
                     </div>
                     <div className="font-gray text-left">
                     <div class="item" style={{backgroundImage:'url("/pic/open_pic/002png.PNG")'}}>
@@ -132,12 +132,12 @@ items.forEach((item, i) => {
                     <div className="font-gray text-left">
                     <div class="item" style={{backgroundImage:'url("/pic/open_pic/003png.PNG")'}}>
                         </div>
-                        <h5 className="font-size-20 margin-0 margin-top-10">도서 전문가</h5>
+                        <h5 className="font-size-20 margin-0 margin-top-10">가구 전문가</h5>
                     </div>
                     <div className="font-gray text-left">
                     <div class="item" style={{backgroundImage:'url("/pic/open_pic/001png.PNG")'}}>
                         </div>
-                        <h5 className="font-size-20 margin-0 margin-top-10">카메라 전문가</h5>
+                        <h5 className="font-size-20 margin-0 margin-top-10">디지털 전문가</h5>
                     </div>
                 </div>
                 <div className="page7_last_text"
@@ -149,7 +149,7 @@ items.forEach((item, i) => {
                                 data-aos-mirror="true"
                                 data-aos-once="false"
                                 data-aos-anchor-placement="center-bottom">
-                    당신의 물건은 여전히 그 가치를 품고 있음을 알기에<br/>
+당신의 물건은 여전히 그 가치를 품고 있음을 알기에<br/>
 그 값어치를 할 수 있도록 저희 서랍은 최선을 다합니다.  <br/></div>
 
             </div>
